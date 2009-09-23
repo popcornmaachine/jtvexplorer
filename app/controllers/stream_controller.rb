73255a0769fd425a639c50f501e2e2cefaf819f8
Ammon Bartram <ammon@justin.tv>
2009-09-22 17:08:51 -0700

    Tests added for stream/new_stream_key, register_callback, unregister_callback, and list_callbacks

--- app/controllers/stream_controller.rb
+++ app/controllers/stream_controller.rb
@@ -10,9 +10,41 @@ class StreamController < ApplicationController
     end
   end
   
+  def register_callback
+    if request.method == :post
+      @query = "/stream/register_callback.#{@serialization_method}" 
+      @result = justintv_oauth_two_legged_post(@query, params)
+    end
+  end
+  
+  def unregister_callback
+    if request.method == :post
+      @query = "/stream/unregister_callback.#{@serialization_method}" 
+      @result = justintv_oauth_two_legged_post(@query, params)
+    end
+  end
+  
+  def unregister_callback
+    if request.method == :post
+      @query = "/stream/unregister_callback.#{@serialization_method}" 
+      @result = justintv_oauth_two_legged_post(@query, params)
+    end
+  end
+  
+  def list_callbacks
+    if request.method == :post
+      @query = "/stream/list_callbacks.#{@serialization_method}" 
+      @result = justintv_oauth_two_legged_post(@query, params)
+    end
+  end
   def list
-    p = { "offset" => params[:offset], "limit" => params[:limit] }.to_params
-
+    p = { "offset" => params[:offset], "limit" => params[:limit]}
+    if (params[:namespace] != nil and params[:namespace].strip != "")
+      params[:namespace].strip!
+      p.merge!(:namespace => (params[:namespace]))
+    end
+    p = p.to_params
+    
     if request.method == :post
       if (params[:login] != "") # view just the channel?
         @query = "/stream/list.#{@serialization_method}?channel=#{params[:login]}"
